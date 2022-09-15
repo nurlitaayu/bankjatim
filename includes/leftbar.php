61672a6d5e9d9eccdf12b94825274e95f451af45
nurlitaayu <nurlitaayu62638@gmail.com>
2022-09-15 13:56:48 +0700

    leftbar

--- includes/leftbar.php
+++ includes/leftbar.php
@@ -31,8 +31,8 @@
 					<ul>
 						<li><a href="<?php echo HTTP_SERVER ?>upload/s_planning.php" style="background:#ff4040;">Planning</a></li>
 						<li><a href="<?php echo HTTP_SERVER ?>upload/pmo/s_pmo.php" style="background:#ff4040;">PMOss</a></li>
-						<li><a href="<?php echo HTTP_SERVER ?>supervisor/gov.php" style="background:#ff4040;">GOV</a></li>
-						<li><a href="<?php echo HTTP_SERVER ?>supervisor/security.php" style="background:#ff4040;">Security</a></li>
+						<li><a href="<?php echo HTTP_SERVER ?>upload/supervisor/s_gov.php" style="background:#ff4040;">GOV</a></li>
+						<li><a href="<?php echo HTTP_SERVER ?>upload/supervisor/s_security.php" style="background:#ff4040;">Security</a></li>
 					</ul>
 
 				<?php
