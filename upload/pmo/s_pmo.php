51917afef26e91afb8c39157219d8ae5f714d73c
nurlitaayu <nurlitaayu62638@gmail.com>
2022-09-15 11:25:59 +0700

    supervisor

---
+++ upload/pmo/s_pmo.php
@@ -0,0 +1,116 @@
+<?php include 'filesLogic.php'; ?>
+
+<?php
+$category = $_GET['category'] ?: [];
+
+?>
+<!doctype html>
+<html lang="en" class="no-js">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="theme-color" content="#3e454c">
+
+    <title>PMO</title>
+
+    <?php include('css/styles.php') ?>
+
+    <!-- Datatable CSS -->
+    <!-- <link href='https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css' rel='stylesheet' type='text/css'> -->
+
+    <!-- jQuery Library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!-- Datatable JS -->
+    <!-- <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script> -->
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.12.1/date-1.1.2/datatables.min.css" />
+
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.12.1/date-1.1.2/datatables.min.js"></script>
+
+</head>
+
+<body>
+
+    <?php require_once './includes/header.php'; ?>
+
+    <div class="ts-main-content">
+        <?php require_once './includes/leftbar.php' ?>
+        <div class="content-wrapper">
+            <div class="container">
+                <h1>Laporan Unit Kerja</h1>
+                <div class="row" style="margin-top: 20px;">
+                    <div class="col-lg-4">
+                        <form action="" method="get">
+                                                        <div>
+                                <!-- PMO -->
+                                <input type="checkbox" name="category[]" value="2" id="pMO" <?php echo in_array(1, $category) == true ? "checked" : '' ?> >
+                                <label for="pmo">Laporan Bulanan</label>
+                            </div>
+
+                            <div>
+                                <!-- Quarterly -->
+                                <input type="checkbox" name="category[]" value="2" id="quarterly" <?= in_array(2, $category) ? 'checked' : '' ?>>
+                                <label for="quarterly">Laporan Triwulan</label>
+                            </div>
+
+                            <div>
+                                <!-- Gov -->
+                                <input type="checkbox" name="category[]" value="3" id="gov" <?= in_array(3, $category) ? 'checked' : '' ?>>
+                                <label for="gov">Laporan Tahunan</label>
+                            </div>
+
+
+                            <button type="submit">Cari</button>
+                        </form>
+                    </div>
+
+                    <div class="col-lg-8">
+                        <table class="table table-hover table-border" id="dataTable">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Dokumen</th>
+                                    <th>Tanggal</th>
+                                    <th>User Pengupload</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Loading Scripts -->
+
+    <script src="js/bootstrap-select.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            setTimeout(function() {
+                $('.succWrap').slideUp("slow");
+            }, 3000);
+
+
+            var dataTable = $('#dataTable').DataTable({
+                processing: true,
+                serverSide: true,
+                ordering: true, // Set true agar bisa di sorting
+                ajax: {
+                    url: "supervisor/get_pmo.php?category=<?= join(',', $category) ?>",
+                    type: "GET"
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
---
+++ upload/supervisor/get_pmo.php
@@ -0,0 +1,66 @@
+<?php
+
+// DB table to use
+
+$category = $_GET['category'] ?: join(',', [1, 2, 3]);
+
+$table = <<<EOT
+(
+    SELECT 
+        f.id,
+        f.name,
+        f.tanggal,
+        f.category_id,
+        u.name as user_name
+    FROM files f
+    INNER JOIN users u ON f.user_id = u.id
+) temp
+EOT;
+// $table = 'files';
+
+// Table's primary key
+$primaryKey = 'id';
+
+// Array of database columns which should be read and sent back to DataTables.
+// The `db` parameter represents the column name in the database, while the `dt`
+// parameter represents the DataTables column identifier. In this case simple
+// indexes
+$columns = array(
+    array('db' => 'id', 'dt' => 0),
+    array('db' => 'name', 'dt' => 1),
+    array('db' => 'tanggal', 'dt' => 2),
+    array('db' => 'user_name', 'dt' => 3,),
+    array('db' => 'id', 'dt' => 4, 'formatter' => function ($pmo, $row) {
+        return '
+            <div style="display: flex; ">
+                <form action="change_pmo.php?acc_id=' . $pmo . '" method="post">
+                <i class="fa fa-power-off" style="color:red"></i>
+                </form>
+                <form action="change.pmo.php?reject_id=' . $pmo . '" method="post">
+                <i class="fa fa-trash" style="color:red"></i>
+                </form>
+            </div>
+        
+        ';
+    })
+);
+
+// SQL server connection information
+$sql_details = array(
+    'user' => 'root',
+    'pass' => '',
+    'db'   => 'bankjatim',
+    'host' => 'localhost'
+);
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * If you just want to use the basic configuration for DataTables with PHP
+ * server-side, there is no need to edit below this line.
+ */
+
+require('../ssp.class.php');
+
+echo json_encode(
+    SSP::complex($_GET, $sql_details, $table, $primaryKey, $columns, null, "category_id IN ($category)")
+);
