cbdc1b158bb080b7433a8a40e99484b09897dee2
farkmu45 <farukmicros@outlook.com>
2022-09-17 17:58:26 +0700

    Fix sidebar url

--- includes/leftbar.php
+++ includes/leftbar.php
@@ -1,10 +1,12 @@
 <?php
+session_start();
 $role =  $_SESSION['SESSION_role'];
 ?>
 
 <nav style="background:#ff4040;" class="ts-sidebar">
 	<ul class="ts-sidebar-menu">
 		<li class="ts-label">Main</li>
+		<!-- Don't forget to change the url -->
 		<li>
 			<a href="http://localhost:8000/profile.php"><i class="fa fa-user"></i> &nbsp;Profile</a>
 		</li>
@@ -14,34 +16,30 @@ $role =  $_SESSION['SESSION_role'];
 		<li>
 			<a href="http://localhost:8000/messages.php"><i class="fa fa-envelope"></i> &nbsp;Messages</a>
 		</li>
-		<li class=" ts-account">
-			<a style="background:#c9c8c5; color:black;" href="#"> &nbsp;Unit Kerja</a>
-			<ul>
-				<?php if ($role == 'Operator') { ?>
+		<?php if ($role != 'Viewer') { ?>
+			<li class=" ts-account">
+				<a style="background:#c9c8c5; color:black;" href="#"> &nbsp;Unit Kerja</a>
+				<ul>
+					<?php if ($role == 'Operator') { ?>
 
-					<li><a href="http://localhost:8000/upload/index.php" style="background:#ff4040;">Planning</a></li>
-					<li><a href="http://localhost:8000/upload/pmo/index.php" style="background:#ff4040;">PMO</a></li>
-					<li><a href="http://localhost:8000/upload/gov/index.php" style="background:#ff4040;">GOV</a></li>
-					<li><a href="http://localhost:8000/upload/security/index.php" style="background:#ff4040;">Security</a></li>
+						<li><a href="http://localhost:8000/upload/index.php" style="background:#ff4040;">Planning</a></li>
+						<li><a href="http://localhost:8000/upload/pmo/index.php" style="background:#ff4040;">PMO</a></li>
+						<li><a href="http://localhost:8000/upload/gov/index.php" style="background:#ff4040;">GOV</a></li>
+						<li><a href="http://localhost:8000/upload/security/index.php" style="background:#ff4040;">Security</a></li>
 
-				<?php } elseif ($role == 'Supervisor') { ?>
+					<?php } elseif ($role == 'Supervisor') { ?>
 
-					<li><a href="http://localhost:8000/upload/s_planning.php" style="background:#ff4040;">Planning</a></li>
-					<li><a href="http://localhost:8000/upload/pmo/s_pmo.php" style="background:#ff4040;">PMO</a></li>
-					<li><a href="http://localhost:8000/upload/gov/s_gov.php" style="background:#ff4040;">GOV</a></li>
-					<li><a href="http://localhost:8000/upload/security/s_security.php" style="background:#ff4040;">Security</a></li>
-
-				<?php } elseif ($role == 'Viewer') { ?>
-
-					<li><a href="http://localhost:8000/upload/index.php" style="background:#ff4040;">Planning</a></li>
-					<li><a href="http://localhost:8000/upload/pmo/index.php" style="background:#ff4040;">PMO</a></li>
-					<li><a href="http://localhost:8000/upload/gov/index.php" style="background:#ff4040;">GOV</a></li>
-					<li><a href="http://localhost:8000/upload/security/index.php" style="background:#ff4040;">Security</a></li>
-
-				<?php } ?>
-
-			</ul>
-
-		</li>
+						<li><a href="http://localhost:8000/upload/s_planning.php" style="background:#ff4040;">Planning</a></li>
+						<li><a href="http://localhost:8000/upload/pmo/s_pmo.php" style="background:#ff4040;">PMO</a></li>
+						<li><a href="http://localhost:8000/upload/gov/s_gov.php" style="background:#ff4040;">GOV</a></li>
+						<li><a href="http://localhost:8000/upload/security/s_security.php" style="background:#ff4040;">Security</a></li>
+					<?php } ?>
+				</ul>
+			</li>
+		<?php } else { ?>
+			<li>
+				<a href="http://localhost:8000/listreport.php"><i class="fa fa-envelope"></i> &nbsp;Report List</a>
+			</li>
+		<?php } ?>
 	</ul>
 </nav>
