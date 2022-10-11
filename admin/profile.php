6d92bd9ce8b617d86db1d8ebc4819b93900ec9ca
nurlitaayu <nurlitaayu62638@gmail.com>
2022-10-11 11:05:17 +0700

    profile admin

--- admin/profile.php
+++ admin/profile.php
@@ -7,13 +7,31 @@ if (strlen($_SESSION['alogin']) == 0) {
 } else {
 
 	if (isset($_POST['submit'])) {
+		$file = $_FILES['image']['name'];
+		$file_loc = $_FILES['image']['tmp_name'];
+		$folder = "images/";
+		$new_file_name = strtolower($file);
+		$final_file = str_replace(' ', '-', $new_file_name);
+
 		$name = $_POST['name'];
 		$email = $_POST['email'];
+		$mobileno = $_POST['mobile'];
+		$designation = $_POST['designation'];
+		$idedit = $_POST['editid'];
+		$image = $_POST['image'];
 
-		$sql = "UPDATE users SET username=(:name), email=(:email)";
+		if (move_uploaded_file($file_loc, $folder . $final_file)) {
+			$image = $final_file;
+		}
+
+		$sql = "UPDATE users SET name=(:name), email=(:email), mobile=(:mobileno), designation=(:designation), Image=(:image) WHERE id=(:idedit)";
 		$query = $dbh->prepare($sql);
 		$query->bindParam(':name', $name, PDO::PARAM_STR);
 		$query->bindParam(':email', $email, PDO::PARAM_STR);
+		$query->bindParam(':mobileno', $mobileno, PDO::PARAM_STR);
+		$query->bindParam(':designation', $designation, PDO::PARAM_STR);
+		$query->bindParam(':image', $image, PDO::PARAM_STR);
+		$query->bindParam(':idedit', $idedit, PDO::PARAM_STR);
 		$query->execute();
 		$msg = "Information Updated Successfully";
 	}
@@ -30,7 +48,7 @@ if (strlen($_SESSION['alogin']) == 0) {
 		<meta name="author" content="">
 		<meta name="theme-color" content="#3e454c">
 
-		<title>Edit Admin</title>
+		<title>Edit Profile</title>
 
 		<!-- Font awesome -->
 		<link rel="stylesheet" href="css/font-awesome.min.css">
@@ -70,12 +88,15 @@ if (strlen($_SESSION['alogin']) == 0) {
 			}
 		</style>
 
+
 	</head>
 
 	<body>
 		<?php
-		$sql = "SELECT * from users;";
+		$email = $_SESSION['alogin'];
+		$sql = "SELECT * from users where email = (:email);";
 		$query = $dbh->prepare($sql);
+		$query->bindParam(':email', $email, PDO::PARAM_STR);
 		$query->execute();
 		$result = $query->fetch(PDO::FETCH_OBJ);
 		$cnt = 1;
@@ -85,28 +106,52 @@ if (strlen($_SESSION['alogin']) == 0) {
 			<?php include('includes/leftbar.php'); ?>
 			<div class="content-wrapper">
 				<div class="container-fluid">
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="page-title">Manage Admin</h3>
+					
 							<div class="row">
 								<div class="col-md-12">
 									<div class="panel panel-default">
-										<div class="panel-heading">Edit Info</div>
+										<div style="font-size:20pt;" class="panel-heading"><?php echo htmlentities($_SESSION['SESSION_role']); ?></div>
 										<?php if ($error) { ?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } else if ($msg) { ?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php } ?>
 
 										<div class="panel-body">
 											<form method="post" class="form-horizontal" enctype="multipart/form-data">
+
 												<div class="form-group">
-													<label class="col-sm-2 control-label">Username<span style="color:red">*</span></label>
+													<div class="col-sm-4">
+													</div>
+													<div class="col-sm-4 text-center">
+														<img src="images/<?php echo htmlentities($result->image); ?>" style="width:200px; border-radius:50%; margin:10px;">
+														<input type="file" name="image" class="form-control">
+														<input type="hidden" name="image" class="form-control" value="<?php echo htmlentities($result->image); ?>">
+													</div>
+													<div class="col-sm-4">
+													</div>
+												</div>
+
+												<div class="form-group">
+													<label class="col-sm-2 control-label">Nama<span style="color:red">*</span></label>
 													<div class="col-sm-4">
 														<input type="text" name="name" class="form-control" required value="<?php echo htmlentities($result->name); ?>">
 													</div>
+
 													<label class="col-sm-2 control-label">Email<span style="color:red">*</span></label>
 													<div class="col-sm-4">
 														<input type="email" name="email" class="form-control" required value="<?php echo htmlentities($result->email); ?>">
 													</div>
 												</div>
 
+												<div class="form-group">
+													<label class="col-sm-2 control-label">No Telepon<span style="color:red">*</span></label>
+													<div class="col-sm-4">
+														<input type="text" name="mobile" class="form-control" required value="<?php echo htmlentities($result->mobile); ?>">
+													</div>
+
+													<label class="col-sm-2 control-label">Alamat<span style="color:red">*</span></label>
+													<div class="col-sm-4">
+														<input type="text" name="designation" class="form-control" required value="<?php echo htmlentities($result->designation); ?>">
+													</div>
+												</div>
+												<input type="hidden" name="editid" class="form-control" required value="<?php echo htmlentities($result->id); ?>">
 
 												<div class="form-group">
 													<div class="col-sm-8 col-sm-offset-2">
