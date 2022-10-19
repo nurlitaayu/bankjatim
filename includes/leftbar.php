b2783da99263a30c73700049a9e8d1aa1b71e1a6
nurlitaayu <nurlitaayu62638@gmail.com>
2022-10-19 11:28:53 +0700

    localhost server

--- includes/leftbar.php
+++ includes/leftbar.php
@@ -8,10 +8,10 @@ $role =  $_SESSION['SESSION_role'];
 		<li class="ts-label">Main</li>
 		<!-- Don't forget to change the url -->
 		<li>
-			<a href="http://localhost:8000/profile.php"><i class="fa fa-user"></i> &nbsp;Profile</a>
+			<a href="http://localhost/bankjatim/profile.php"><i class="fa fa-user"></i> &nbsp;Profile</a>
 		</li>
 		<li>
-			<a href="http://localhost:8000/notification.php"><i class="fa fa-bell"></i> &nbsp;Notification<sup style="color:red">*</sup></a>
+			<a href="http://localhost/bankjatim/notification.php"><i class="fa fa-bell"></i> &nbsp;Notification<sup style="color:red">*</sup></a>
 		</li>
 
 		<li class=" ts-account">
@@ -19,24 +19,24 @@ $role =  $_SESSION['SESSION_role'];
 			<ul>
 				<?php if ($role == 'Operator') { ?>
 
-					<li><a href="http://localhost:8000/upload/index.php" style="background:#ff4040;">Planning</a></li>
-					<li><a href="http://localhost:8000/upload/pmo/index.php" style="background:#ff4040;">PMO</a></li>
-					<li><a href="http://localhost:8000/upload/gov/index.php" style="background:#ff4040;">GOV</a></li>
-					<li><a href="http://localhost:8000/upload/security/index.php" style="background:#ff4040;">Security</a></li>
+					<li><a href="http://localhost/bankjatim/upload/index.php" style="background:#ff4040;">Planning</a></li>
+					<li><a href="http://localhost/bankjatim/upload/pmo/index.php" style="background:#ff4040;">PMO</a></li>
+					<li><a href="http://localhost/bankjatim/upload/gov/index.php" style="background:#ff4040;">GOV</a></li>
+					<li><a href="http://localhost/bankjatim/upload/security/index.php" style="background:#ff4040;">Security</a></li>
 
 				<?php } elseif ($role == 'Supervisor') { ?>
 
-					<li><a href="http://localhost:8000/upload/s_planning.php" style="background:#ff4040;">Planning</a></li>
-					<li><a href="http://localhost:8000/upload/pmo/s_pmo.php" style="background:#ff4040;">PMO</a></li>
-					<li><a href="http://localhost:8000/upload/gov/s_gov.php" style="background:#ff4040;">GOV</a></li>
-					<li><a href="http://localhost:8000/upload/security/s_security.php" style="background:#ff4040;">Security</a></li>
+					<li><a href="http://localhost/bankjatim0/upload/s_planning.php" style="background:#ff4040;">Planning</a></li>
+					<li><a href="http://localhost/bankjatim/upload/pmo/s_pmo.php" style="background:#ff4040;">PMO</a></li>
+					<li><a href="http://localhost/bankjatim/upload/gov/s_gov.php" style="background:#ff4040;">GOV</a></li>
+					<li><a href="http://localhost/bankjatim/upload/security/s_security.php" style="background:#ff4040;">Security</a></li>
 					
 				<?php } elseif ($role == 'Viewer') { ?>
 
-					<li><a href="http://localhost:8000/upload/v_planning.php" style="background:#ff4040;">Planning</a></li>
-					<li><a href="http://localhost:8000/upload/pmo/v_pmo.php" style="background:#ff4040;">PMO</a></li>
-					<li><a href="http://localhost:8000/upload/gov/v_gov.php" style="background:#ff4040;">GOV</a></li>
-					<li><a href="http://localhost:8000/upload/security/v_security.php" style="background:#ff4040;">Security</a></li>
+					<li><a href="http://localhost/bankjatim/upload/v_planning.php" style="background:#ff4040;">Planning</a></li>
+					<li><a href="http://localhost/bankjatim/upload/pmo/v_pmo.php" style="background:#ff4040;">PMO</a></li>
+					<li><a href="http://localhost/bankjatim/upload/gov/v_gov.php" style="background:#ff4040;">GOV</a></li>
+					<li><a href="http://localhost/bankjatim/upload/security/v_security.php" style="background:#ff4040;">Security</a></li>
 				<?php } ?>
 			</ul>
 		</li>
